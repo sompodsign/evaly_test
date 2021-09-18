670e75c789907af38ad87eccf2fa2cfc09ed259e
Shampad Sharkar <sompodsign@gmail.com>
2021-09-18 22:19:25 +0600

    refactor some code

--- tests/test_evaly.py
+++ tests/test_evaly.py
@@ -6,7 +6,7 @@ from utils.test_cases import test_cases
 class TestEvaly(BaseTest):
 
     # Case 1
-    def test_01_sign_in(self):
+    def test_01_sign_in(self): # TODO 0: Look for the solution of webdriver getting detected.
         print("\n" + str(test_cases(0)))
         login_modal = MainPage(self.driver).click_user_icon()
         login_modal.login() # doesn't login as server detects selenium webdriver
@@ -18,13 +18,13 @@ class TestEvaly(BaseTest):
         speaker_page.grab_brands()
         self.assertIn('categories/speaker', speaker_page.get_url())
 
-    def test_03_products(self):
+    def test_03_products(self): #TODO 1: Create excel from list of dictionaries.
         speaker_page = HomePage(self.driver).click_speaker_link()
         speaker_page.grab_products()
 
-    # def test_03_compare_price(self):
+    # def test_03_compare_price(self): #TODO 2: Price compare for some products with details page.
     #     pass
     #
     # # Case 3
-    # def test_04_career_page(self):
+    # def test_04_career_page(self): #TODO3: Navigate to career page and assert email domains.
     #     pass
